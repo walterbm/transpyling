7e43892db088047e8d52ae688e86ca3110a6c96c
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-03-31 22:23:09 -0400

    clean up ruby transpiling

--- magic.sh
+++ magic.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 echo "Starting with Ruby:"
+printf "number of lines: "
+wc -l < ruby/pie.rb
 ruby ruby/pie.rb
 echo
 echo "Ruby --> Javascript ☕:"
+printf "number of lines: "
+wc -l < javascript/ruby-pie.js
 rake -f ./ruby/rakefile.rb build
-node javascript/pie.js
+node javascript/ruby-pie.js
 echo
--- ruby/rakefile.rb
+++ ruby/rakefile.rb
@@ -3,5 +3,5 @@ require 'opal'
 desc "Convert Ruby to JavaScript"
 task :build do
   Opal.append_path "ruby"
-  File.binwrite "./javascript/pie.js", Opal::Builder.build("pie").to_s
+  File.binwrite "./javascript/ruby-pie.js", Opal::Builder.build("pie").to_s
 end
