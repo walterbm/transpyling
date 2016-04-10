b3296e557a5783de70a4e8b55a525cf1de934631
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-04-09 21:25:03 -0400

    clean up magic transpiling script

--- magic.sh
+++ magic.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
 bash conversion-scripts/python2js.sh
-
 sleep 4
-
 bash conversion-scripts/ruby2js.sh
-
 sleep 4
-
 bash conversion-scripts/js2php.sh
-
 sleep 4
-
 bash conversion-scripts/js2python.sh
-
 sleep 4
-
 bash conversion-scripts/js2hodor.sh
-
 sleep 4
-
 bash conversion-scripts/doge2js.sh
